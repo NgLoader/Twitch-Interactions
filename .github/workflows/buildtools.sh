550767df98f0a90e336e2b28294c25c93ebf30f3
Nils Gereke <nilsgereke1@gmail.com>
2023-12-21 07:25:32 +0100

    feat: add github actions

---
+++ .github/workflows/buildtools.sh
@@ -0,0 +1,24 @@
+# Author
+# Imprex-Development
+
+build () {
+	echo Building $1 ...
+	rm -rf $1
+    mkdir $1
+    cd $1
+
+    curl -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+    java -jar BuildTools.jar --rev $1 --remapped
+
+    cd ..
+}
+
+checkVersion () {
+	echo Checking version $1
+
+	if [ ! -d ~/.m2/repository/org/spigotmc/spigot/$1-R0.1-SNAPSHOT ]; then
+		build $1
+	fi
+}
+
+checkVersion 1.20.2
